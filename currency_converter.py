19b3135f9524983f7bd83a57c6fc920ade021233
mikey0830 <lpgb8515@leeds.ac.uk>
2024-11-01 14:59:30 +0000

    Add currency_converter.py file

--- currency_converter.py
+++ currency_converter.py
@@ -1,13 +1,21 @@
-defcurrency converter(amount,from currency,to currency):
+def currency_converter(amount, from_currency, to_currency):
+    
+    conversion_rate = {
+        'CNY': {'GBP': 0.11, 'PHP': 8.12, 'INR': 11.81},
+        'INR': {'CNY': 0.09, 'PHP': 8.69, 'GBP': 0.01},
+        'GBP': {'CNY': 9.24, 'PHP': 75.00, 'INR': 109.06},
+        'PHP': {'CNY': 0.12, 'GBP': 0.01, 'INR': 1.45}
+    }
 
-N
-conversion rate=
-{'CNY': 9.24'INR': 109.06}，GBP':'PHP':75.00,
-CNY':{'GBP':0.11,'PHP':8.12,'INR': 11.81},
-'PHP':{'CNY': 0.12,'GBP': 0.01,INR': 1.45}，
-'GBP':0.01}INR':「'PHP':8.69.'CNY': o.09.
+   
+    if amount < 0:
+        return 0.0
 
-if amount< 0:
-return 0.0
-else:
-converted amount =amount *conversion ratelfrom currencyl[to currencyireturn round(converted amount，2)
+    
+    if from_currency in conversion_rate and to_currency in conversion_rate[from_currency]:
+        converted_amount = amount * conversion_rate[from_currency][to_currency]
+        return round(converted_amount, 2)
+    else:
+        
+        return None
+
