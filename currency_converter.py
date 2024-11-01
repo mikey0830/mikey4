e4adf91e66889f74cfd2c2dd91380d5cab55c54e
mikey0830 <lpgb8515@leeds.ac.uk>
2024-11-01 14:54:33 +0000

    Add currency_converter.py file

---
+++ currency_converter.py
@@ -0,0 +1,13 @@
+defcurrency converter(amount,from currency,to currency):
+
+N
+conversion rate=
+{'CNY': 9.24'INR': 109.06}，GBP':'PHP':75.00,
+CNY':{'GBP':0.11,'PHP':8.12,'INR': 11.81},
+'PHP':{'CNY': 0.12,'GBP': 0.01,INR': 1.45}，
+'GBP':0.01}INR':「'PHP':8.69.'CNY': o.09.
+
+if amount< 0:
+return 0.0
+else:
+converted amount =amount *conversion ratelfrom currencyl[to currencyireturn round(converted amount，2)
